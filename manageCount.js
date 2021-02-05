18d14f592304e839cf24041e754227cd25f88610
William Jones <42547062+CodeJonesW@users.noreply.github.com>
2021-02-05 15:12:37 -0500

    update

--- manageCount.js
+++ manageCount.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", () => {
     const display = document.getElementById("count")
     let count = localStorage.getItem("counted")
-    
+//    newExample()
     if(count !== null){
         display.innerHTML = "Count: " + count
     } else {
@@ -22,8 +22,8 @@ window.addEventListener("load", () => {
             case "clear":
                 clear()
                 break;
-            case "array":
-                arrayExample()
+            case "object":
+                objectExample()
                 break;
         }
     })
@@ -45,21 +45,27 @@ window.addEventListener("load", () => {
     }
 
     const clear = () => {
+        localStorage.clear()
         localStorage.setItem("counted", 0)
         count = localStorage.getItem("counted")
         console.log(localStorage)
         display.innerHTML = "Count: " + count
     }
 
-    const arrayExample = () => {
-        localStorage.setItem("myArray", JSON.stringify([1,2,3]))
-
-        console.log(localStorage)
-
-        let newArray = JSON.parse(localStorage.getItem("myArray"))
-        console.log(typeof newArray)
-        let newArrayElement = document.getElementById("newArray")
-        newArrayElement.innerHTML = newArray
-
-    }
 })
+
+
+let myObject = {
+    user: "mainUser"
+}
+
+
+const objectExample = () => {
+    let myObject = {name: "Will"}
+    let myArray = ["Will", "Robert"]
+    localStorage.setItem("myObject", JSON.stringify(myObject))
+    console.log(localStorage)
+    let myOtherObject = JSON.parse(localStorage.getItem("myObject"))
+    
+    console.log(myOtherObject)
+}  
